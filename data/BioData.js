568ccf3fe141071ee542e6b9d3a054727d45e653
bhaskoro.muthohar <bhaskoro.jr@gmail.com>
2025-03-29 16:32:00 +0700

    feat: Update BioData.js with new description

--- data/BioData.js
+++ data/BioData.js
@@ -1,24 +1,24 @@
 // date: 29 Mar, 2025
 const bioData = [
-    {
-        name: 'Bhaskoro Muthohar',
-        username: 'Bhas',
-        url: 'https://itsmebhas.net',
-        titleImg: false,
-        avatar: '/1693816359351.jpeg',
-        nftAvatar: true,
-        description: 'Analytics Engineer with expertise in data engineering and machine learning. Currently a Data Engineer at StraitsX, focusing on maintaining ML infrastructure. Previously @ Govtech Edu IDN and Bank Jago.',
-        descShow: true,
-        subdesc: 'Machine Learning Engineering (Model Deployment & MLOps) \nData Engineering (Python, SQL, GCP, Kubernetes, CI/CD) \nAnalytics Engineering (dbt, Airflow, BigQuery)',        
-        subdescShow: true,
-        newProductUrl: 'https://github.com/bhaskoro-muthohar/RealEstimate',
-        newProduct: true,
-        footerText: '© 2025 Bhaskoro Muthohar',
-        author: '',
-        authorURL: ''
-    },
+  {
+    name: "Bhaskoro Muthohar",
+    username: "Bhas",
+    url: "https://itsmebhas.net",
+    titleImg: false,
+    avatar: "/1693816359351.jpeg",
+    nftAvatar: true,
+    description:
+      "Data Engineer at StraitsX, responsible for managing machine learning infrastructure. Formerly with GovTech Edu Indonesia and Bank Jago.",
+    descShow: true,
+    subdesc:
+      "Machine Learning Engineering (Model Deployment & MLOps) \nData Engineering (Python, SQL, GCP, Kubernetes, CI/CD) \nAnalytics Engineering (dbt, Airflow, BigQuery)",
+    subdescShow: true,
+    newProductUrl: "https://github.com/bhaskoro-muthohar/RealEstimate",
+    newProduct: true,
+    footerText: "© 2025 Bhaskoro Muthohar",
+    author: "",
+    authorURL: "",
+  },
 ];
 
 export default bioData;
-
-
